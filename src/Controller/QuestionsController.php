e7eb2a01c99449915d928be0d83c022aa48cf339
asakon <kondoasako@gmail.com>
2020-04-07 11:39:56 +0900

    質問投稿画面

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -16,4 +16,19 @@ class QuestionsController extends AppController
      ]);
      $this->set(compact('questions'));
   }
+
+  /*
+  * @return \Cake\Http\Response | null 質問投稿時に質問一覧画面へ遷移する
+  */
+  public function add()
+  {
+    $question = $this->Questions->newEntity();
+
+    if ($this->request->is('post')) {
+      // フォームの処理を書く 
+    }
+
+    $this->set(compact('question'));
+  }
+  
 }
